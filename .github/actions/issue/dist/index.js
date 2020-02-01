844566173f68b260d1137cf0dbbb4a5708f5fcae
Ali Alaa <hello@alialaa.com>
2020-02-01 18:21:55 +0200

    rofll

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -7250,13 +7250,24 @@ const github = __webpack_require__(469);
 try {
   // `who-to-greet` input defined in action metadata file
   // throw(new Error('No'))
+  core.setSecret(nameToGreet);
+
+  core.debug('Getting Input');
+  core.warning('nameToGreet was not set');
+  core.error(`Error`);
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github, null, '\t')
+  
+  core.startGroup('Do some function')
   console.log(`The event payload: ${payload}`);
+  console.log(`Hello`);
+  core.endGroup()
+
+  core.exportVariable('HELLO', time);
 } catch (error) {
   core.setFailed(error.message);
 }
