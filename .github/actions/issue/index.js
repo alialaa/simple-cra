2fbcf44384d1d56b30c9763059bba610742d833b
Ali Alaa <hello@alialaa.com>
2020-02-01 16:29:23 +0200

    rofll

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -3,11 +3,11 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
+  throw(new Error('No'))
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   // const time = (new Date()).toTimeString();
   core.setOutput("time", new Date());
-  throw(new Error('No'))
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github, null, '\t')
   console.log(`The event payload: ${payload}`);
