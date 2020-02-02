8beb799cf5d74bf55d6923a88c87b5c175f37cc7
Ali Alaa <hello@alialaa.com>
2020-02-02 10:19:56 +0200

    rofll

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -7252,16 +7252,16 @@ async function run() {
     const token = core.getInput('token');
     const octokit = new github.GitHub(token);
   
-    const issue = await octokit.issues.create({
+    const response = await octokit.issues.create({
       // owner: github.context.repo.owner,
       // repo: github.context.repo.repo,
       ...github.context.repo,
-      title: 'New issue!',
+      // title: 'New issue!',
       body: 'Hello Universe!',
       assignees: ["alialaa"]
     });
 
-    console.log(issue);
+    console.log(response.data);
   } catch (error) {
     core.setFailed(error.message);
   }
