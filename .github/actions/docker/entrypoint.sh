990dc86a555acacbf3a558aae1d47c0d9d659993
Ali Alaa <hello@alialaa.com>
2020-02-03 18:24:49 +0200

    drawer menu

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -4,7 +4,7 @@ echo "::debug ::Here's a debug message"
 echo "::warning ::Here's a warning message"
 echo "::error ::Here's a error message"
 
-echo "::add-mask::$1"
+# echo "::add-mask::$1"
 
 echo "Hello $1"
 time=$(date)
