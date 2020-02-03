fe7f062b8495a0b9047b264c4d6bf73819802ff4
Ali Alaa <hello@alialaa.com>
2020-02-03 18:27:20 +0200

    drawer menu

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
+if [ $1 == "Mona the Octocat" ] 
+then
+  echo "error message"
+  exit 1
+fi
+
 echo "::debug ::Here's a debug message"
 echo "::warning ::Here's a warning message"
 echo "::error ::Here's a error message"
@@ -17,7 +23,4 @@ echo '::endgroup::'
 
 echo "::set-env name=HELLO::$time"
 
-if [ $1 == 'Mona the Octocat' ] ; then
-  echo "::error ::Here's a error message"
-  exit 1
-fi
+
