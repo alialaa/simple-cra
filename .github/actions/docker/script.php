348922ad65d48c5b957c2a93c5c43ae3bdc3c789
Ali Alaa <hello@alialaa.com>
2020-02-03 20:25:40 +0200

    drawer menu

---
+++ .github/actions/docker/script.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'vendor/autoload.php';
+
+Requests::register_autoloader();
+
+$response = Requests::post(
+    'https://hooks.slack.com/services/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', 
+    array(
+        'Content-type' => 'application/json'
+    ),
+    json_encode(array(
+        "text" => "Hello, World!"
+    ))
+);
+
+// var_dump($response->body);
+// $output = shell_exec('echo "Lolll"');
+// echo "<pre>$output</pre>";
+
+if(!$response->success) {
+    echo 'error';
+}
