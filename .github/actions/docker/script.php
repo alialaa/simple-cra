892eb227dd3b8a80f06a60ff1119fe5e24894c56
Ali Alaa <hello@alialaa.com>
2020-02-04 09:18:26 +0200

    drawer menu

--- .github/actions/docker/script.php
+++ .github/actions/docker/script.php
@@ -11,8 +11,39 @@ $response = Requests::post(
     array(
         'Content-type' => 'application/json'
     ),
-    json_encode(array(
-        "text" => $_ENV['INPUT_MESSAGE']
+    json_encode(array (
+        // 'text' => 'Hello',
+        'blocks' => 
+            array (
+                array (
+                    "type" => "section",
+                    "text" => array (
+                        "type" => "mrkdwn",
+                        "text" => $_ENV['INPUT_MESSAGE'],
+                    ),
+                ),
+                array (
+                    "type" => "section",
+                    "fields" => array (
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*Repository:*\n${$_ENV['GITHUB_REPOSITORY']}",
+                        ),
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*Event:*\n${$_ENV['GITHUB_EVENT_NAME']}",
+                        ),
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*Ref:*\n${$_ENV['GITHUB_REF']}",
+                        ),
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*SHA:*\n${$_ENV['GITHUB_SHA']}",
+                        ),
+                    ),
+                ),
+            ),
     ))
 );
 
